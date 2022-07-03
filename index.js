db68d48b5bcc2d1da82b5867ed1acd54d3a8f817
sukithaj <97999466+sukithaj@users.noreply.github.com>
2022-07-03 10:41:54 +0530

    Test quality gate

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('Hello World !!!');
+'use strict';
+let a = 1.0;
+let b = '1'
+const c = 1
+
+console.log(typeof a);
+
+b.prop = function(){
+    console.log(`I am A`);
+}
+
+console.log(b === c);
+console.log(a === c);
+console.log(b === a);
+
+a.print();
+
