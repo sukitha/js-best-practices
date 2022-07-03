a9fc95e7d241683d5234b803f7257e6dbde918e1
sukithaj <97999466+sukithaj@users.noreply.github.com>
2022-07-03 11:34:35 +0530

    fix sonaLint issues

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-'use strict';
+"use strict";
 
-const a = 1.0;
-const b = '1'
-const c = 1
+let a;
+a = 1.0;
+let b;
+b = "1";
+let c;
+c = 1;
 
 console.log(typeof a);
 
-b.prop = function(){
-    console.log(`I am A`);
-}
+b.prop = function () {
+  console.log(`I am A`);
+};
 
 console.log(b === c);
 console.log(a === c);
 console.log(b === a);
 
 a.print();
-
-
